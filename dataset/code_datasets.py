fix masking

dataset/code_datasets.py
@@ -27,18 +27,10 @@ class CodeRefactoringDataset(torch.utils.data.Dataset):
         code_string = self.samples.iloc[idx].func_code_string
         code_label = self.tokenizer(code_string)
 
-        num_tokens_to_mask = (len(code_label) * self.mask_percentage) // 100
-
         code_tree = self.parser.parse(bytes(code_string, "utf-8"))
 
         code_identifiers = self._get_identifiers(code_tree.root_node)
-
-        # use all identifiers in case there is less than number of tokens
-        num_tokens_to_mask = (
-            num_tokens_to_mask
-            if len(code_identifiers) > num_tokens_to_mask
-            else len(code_identifiers)
-        )
+        num_tokens_to_mask = (len(code_identifiers) * self.mask_percentage) // 100
 
         sampled_identifiers = self.sample_identifiers(
             code_identifiers, num_tokens_to_mask

utils/collate.py
@@ -1,3 +1,4 @@
+import torch
 from torch.nn.utils.rnn import pad_sequence
 
 
@@ -7,11 +8,11 @@ def collate_fn(batch, pad_id):
     labels = []
 
     for sample in batch:
-        masked_inputs.append(sample["masked_input"]["input_ids"].squeeze(0))
+        masked_inputs.append(torch.tensor(sample["masked_input"]["input_ids"]))
         masked_inputs_attention.append(
-            sample["masked_input"]["attention_mask"].squeeze(0)
+            torch.tensor(sample["masked_input"]["attention_mask"])
         )
-        labels.append(sample["label"]["input_ids"].squeeze(0))
+        labels.append(torch.tensor(sample["label"]["input_ids"]))
 
     padded_input = pad_sequence(masked_inputs, padding_value=pad_id, batch_first=True)
     padded_input_attention = pad_sequence(