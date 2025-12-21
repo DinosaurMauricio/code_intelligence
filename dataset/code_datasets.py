b097796137290b1569858eba7be576233066d0df
Mauricio <mauricio.eduardo.nr@gmail.com>
2025-12-21 03:26:05 +0100

    encode srting to utf-8 to avoid errors in dataset

--- dataset/code_datasets.py
+++ dataset/code_datasets.py
@@ -77,44 +77,57 @@ class CodeRefactoringDataset(torch.utils.data.Dataset):
         sampled_identifiers = sorted(sampled_identifiers, key=lambda x: x[0])
         return sampled_identifiers
 
+    # TODO: Clean this method
     def mask_identifiers(self, code_string, masks):
         labels = []
         offset = 0
 
+        # tree-sitter works with bytes, when working with english code is fine
+        # if we use the relative byte positon, but when working for example
+        # with chinese characters the position might differ, encoding it
+        # fixes this issue
+        code_bytes = code_string.encode("utf8")
+        mask_holder = "<mask_holder>".encode("utf-8")
+
         for mask in masks:  # masks are the identifiers
             start, end = mask[0], mask[1]
 
-            if code_string[start + offset - 1] == " ":
+            if code_bytes[start + offset - 1] == 32:  # 32 is ' ' in bytes
                 # This is because the tokenizer before a space it might tokenize it together for example kwargs without
                 # space is kw, args. if  before there was a space it would make it Ġk, wa, rgs which is completly differnt
                 # in this case we just attach the space if before there was a space
                 start -= 1
 
-            identifier_label = code_string[start + offset : end + offset]
-            labels.append(identifier_label)
+            identifier_label = code_bytes[start + offset : end + offset]
+            labels.append(identifier_label.decode("utf-8"))
 
             # set a temporary token to get the position, this is because if we mask and
             # then tokenize, its more difficult to find the positions. This way we can
             # add a place holder token on the identifiers and after tokenizing it
             # its possible to change the holder with the true tokenized identifiers
-            code_string = (
-                code_string[: start + offset]
-                + "<mask_holder>"
-                + code_string[end + offset :]
+            code_bytes = (
+                code_bytes[: start + offset] + mask_holder + code_bytes[end + offset :]
             )
 
             difference = end - start
-            offset += len("<mask_holder>") - difference
+            offset += len(mask_holder) - difference
 
+        code_string = code_bytes.decode("utf-8")
         tokenized_code = self.tokenizer.tokenize(code_string)
 
         index = 0
+        # TODO: Check here because it seems its eating up some tokens....
+        # the labels should be already be ordered but it might be that reason why
+        # or either the mask_holder is not being set
         while len(labels) > 0:
             token = tokenized_code[index]
             if token == "<mask_holder>":
                 identifier = labels.pop(0)
                 ## TODO: Worth the shot just taking the first token of the label when it tokenizes
                 ## for example long variables and just masks part of it
+
+                print(self.tokenizer.tokenize(identifier))
+                print(identifier)
                 tokenized_code = (
                     tokenized_code[:index]
                     + [self.tokenizer.mask_token]
