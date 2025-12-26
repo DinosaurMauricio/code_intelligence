3c168ca80fc4f313758def363e3a1564ab184c21
Mauricio <mauricio.eduardo.nr@gmail.com>
2025-12-26 13:55:40 +0100

    clean a bit the code

--- dataset/code_datasets.py
+++ dataset/code_datasets.py
@@ -1,7 +1,8 @@
-import random
 import torch
 import pandas as pd
+
 from utils.parser import CodeParser
+from utils.processor import CodeMaskingProcessor
 
 
 class CodeRefactoringDataset(torch.utils.data.Dataset):
@@ -14,13 +15,10 @@ class CodeRefactoringDataset(torch.utils.data.Dataset):
     ):
         self.samples = data
         self.tokenizer = tokenizer
-        # add to tokenizer vocabulary <mask_holder>
-        self.tokenizer.add_tokens(["<mask_holder>"])
-
         self.parser = parser
         self.mask_percentage = mask_percentage
-
-        self.tokenizer_mask = tokenizer.mask_token
+        # this could be injected but because only using python right now this is fine
+        self.code_masker = CodeMaskingProcessor(tokenizer)
 
     def __len__(self):
         return len(self.samples)
@@ -28,125 +26,25 @@ class CodeRefactoringDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         code_string = self.samples.iloc[idx].func_code_string
 
-        # parse the code string
+        # parse the code string, needs to be bytes
         code_tree = self.parser.parse(bytes(code_string, "utf-8"))
 
-        code_identifiers = self._get_identifiers(code_tree.root_node)
+        code_identifiers = self.code_masker.extract_identifiers(code_tree.root_node)
+
         num_tokens_to_mask = (len(code_identifiers) * self.mask_percentage) // 100
 
-        sampled_identifiers = self.sample_identifiers(
+        sampled_identifiers = self.code_masker.sample_identifiers(
             code_identifiers, num_tokens_to_mask
         )
 
-        masked_string = self.mask_identifiers(code_string, sampled_identifiers)
+        masked_input = self.code_masker.apply_masking(
+            code_string,
+            sampled_identifiers,
+        )
 
         sample = {
-            "label": self.samples.iloc[idx].func_code_string,
-            "masked_input": masked_string,
+            "label": code_string,
+            "masked_input": masked_input,
         }
 
         return sample
-
-    @staticmethod
-    def _get_identifiers(root_node):
-        identifiers = []
-
-        def tree_traversal(node):
-            for children_node in node.children:
-                # identifiers hold variables and method names
-                if children_node.type == "identifier":
-                    # get the string position of each identifier
-                    identifiers.append(
-                        (children_node.start_byte, children_node.end_byte)
-                    )
-
-                if children_node.type == "comment":
-                    # Ignore comments, maybe the dataset could be preprocessed and remove comments...
-                    continue
-
-                tree_traversal(children_node)
-
-        tree_traversal(root_node)
-
-        return identifiers
-
-    @staticmethod
-    def sample_identifiers(identifiers, num_masks):
-        sampled_identifiers = random.sample(identifiers, num_masks)
-        # need to sort the values as to modify the string from left to right
-        sampled_identifiers = sorted(sampled_identifiers, key=lambda x: x[0])
-        return sampled_identifiers
-
-    def mask_identifiers(self, code_string, masks):
-        labels = []
-        offset = 0
-
-        # tree-sitter works with bytes, when working with english code is fine
-        # if we use the relative byte positon, but when working for example
-        # with chinese characters the position might differ, encoding it
-        # fixes this issue
-        code_bytes = code_string.encode("utf8")
-        mask_holder = "<mask_holder>".encode("utf-8")
-
-        for mask in masks:  # masks are the identifiers
-            start, end = mask[0], mask[1]
-
-            if code_bytes[start + offset - 1] == 32:  # 32 is ' ' in bytes
-                # This is because the tokenizer before a space it might tokenize it together for example kwargs without
-                # space is kw, args. if  before there was a space it would make it Ġk, wa, rgs which is completly differnt
-                # in this case we just attach the space if before there was a space
-                start -= 1
-
-            identifier_label = code_bytes[start + offset : end + offset]
-            labels.append(identifier_label.decode("utf-8"))
-
-            # set a temporary token to get the position, this is because if we mask and
-            # then tokenize, its more difficult to find the positions. This way we can
-            # add a place holder token on the identifiers and after tokenizing it
-            # its possible to change the holder with the true tokenized identifiers
-            code_bytes = (
-                code_bytes[: start + offset] + mask_holder + code_bytes[end + offset :]
-            )
-
-            difference = end - start
-            offset += len(mask_holder) - difference
-
-        code_string = code_bytes.decode("utf-8")
-        tokenized_code = self.tokenizer.tokenize(code_string)
-
-        index = 0
-        # TODO: Check here because it seems its eating up some tokens....
-        # the labels should be already be ordered but it might be that reason why
-        # or either the mask_holder is not being set
-        while len(labels) > 0:
-            token = tokenized_code[index]
-            if token == "<mask_holder>":
-                identifier = labels.pop(0)
-                ## TODO: Worth the shot just taking the first token of the label when it tokenizes
-                ## for example long variables and just masks part of it
-
-                tokenized_code = (
-                    tokenized_code[:index]
-                    + [self.tokenizer.mask_token]
-                    * len(self.tokenizer.tokenize(identifier))
-                    + tokenized_code[index + 1 :]
-                )
-            index += 1
-
-        max = 256
-
-        if len(tokenized_code) >= max - 2:
-            tokenized_code = tokenized_code[
-                : max - 2
-            ]  #  truncate it. -2 because need to add bos and eos token
-
-        tokenized_code = (
-            [self.tokenizer.bos_token] + tokenized_code + [self.tokenizer.eos_token]
-        )
-
-        padding = [self.tokenizer.pad_token] * (max - len(tokenized_code))
-        tokenized_code = tokenized_code + padding
-
-        tokenized_code = self.tokenizer.convert_tokens_to_ids(tokenized_code)
-
-        return tokenized_code
---
+++ utils/processor.py
@@ -0,0 +1,129 @@
+import random
+
+
+class CodeMaskingProcessor:
+
+    MASK_PLACEHOLDER = "<mask_holder>"
+    SPACE_BYTE = 32
+
+    def __init__(self, tokenizer, max_length=256):
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    @staticmethod
+    def extract_identifiers(root_node):
+        identifiers = []
+
+        def tree_traversal(node):
+            for children_node in node.children:
+                # identifiers hold variables and method names
+                if children_node.type == "identifier":
+                    # get the string position of each identifier
+                    identifiers.append(
+                        (children_node.start_byte, children_node.end_byte)
+                    )
+
+                if children_node.type == "comment":
+                    # Ignore comments, maybe the dataset could be preprocessed and remove comments...
+                    continue
+
+                tree_traversal(children_node)
+
+        tree_traversal(root_node)
+
+        return identifiers
+
+    @staticmethod
+    def sample_identifiers(identifiers, num_masks):
+        sampled_identifiers = random.sample(identifiers, num_masks)
+        # need to sort the values to modify the string from left to right
+        sampled_identifiers = sorted(sampled_identifiers, key=lambda x: x[0])
+        return sampled_identifiers
+
+    def apply_masking(self, code_string, masks_positions):
+
+        original_identifiers, code_with_placeholders = self._insert_placeholders(
+            code_string,
+            masks_positions,
+        )
+
+        code_tokens = self.tokenizer.tokenize(code_with_placeholders)
+
+        masked_tokens = self._replace_placeholders_with_masks(
+            original_identifiers,
+            code_tokens,
+        )
+
+        padded_tokens = self._add_special_tokens_and_padding(masked_tokens)
+
+        return self.tokenizer.convert_tokens_to_ids(padded_tokens)
+
+    def _insert_placeholders(self, masks_positions, code_string: str):
+
+        # tree-sitter works with bytes, when working with english codes its fine
+        # if we use the relative byte positon, but when working for example
+        # with chinese characters the position might differ, encoding it
+        # fixes this issue
+        code_bytes = code_string.encode("utf8")
+        mask_holder = self.MASK_PLACEHOLDER.encode("utf-8")
+
+        offset = 0
+        labels = []
+
+        for start, end in masks_positions:  # positions are in bytes
+            if code_bytes[start + offset - 1] == self.SPACE_BYTE:  # 32 is ' ' in bytes
+                # This is because the tokenizer might tokenize the word with the space, for example:
+                # kwargs without space is [kw, args] with a space in the previous position
+                # [Ġk, wa, rgs] which is completly different. For this reason,
+                # in these cases i attach the space into the identifier
+                start -= 1
+
+            identifier_label = code_bytes[start + offset : end + offset]
+            labels.append(identifier_label.decode("utf-8"))
+
+            # set a temporary token (i.e "<mask_holder>"). This is because if we mask and
+            # then tokenize, its more difficult to find the positions of the words.
+            # by placing placeholder token on the identifiers position its more simple to
+            # find the positions of the masks and ground truth labels
+            code_bytes = (
+                code_bytes[: start + offset] + mask_holder + code_bytes[end + offset :]
+            )
+
+            # because we modify the length of the string we need an offset for the positions
+            difference = end - start
+            offset += len(mask_holder) - difference
+
+        code_string = code_bytes.decode("utf-8")
+        return labels, code_string
+
+    def _replace_placeholders_with_masks(self, labels, tokenized_code):
+        index = 0
+        while len(labels) > 0:
+            token = tokenized_code[index]
+            if token == self.MASK_PLACEHOLDER:
+                identifier = labels.pop(0)
+                ## TODO: Worth the shot just taking the first token of the label when it tokenizes
+                ## for example long variables and just masks part of it
+                tokenized_code = (
+                    tokenized_code[:index]
+                    + [self.tokenizer.mask_token]
+                    * len(self.tokenizer.tokenize(identifier))
+                    + tokenized_code[index + 1 :]
+                )
+            index += 1
+
+        return tokenized_code
+
+    def _add_special_tokens_and_padding(self, tokenized_code):
+
+        if len(tokenized_code) >= self.max_length - 2:
+            #  truncate it. -2 because need to add bos and eos token
+            tokenized_code = tokenized_code[: self.max_length - 2]
+
+        tokenized_code = (
+            [self.tokenizer.bos_token] + tokenized_code + [self.tokenizer.eos_token]
+        )
+
+        padding = [self.tokenizer.pad_token] * (self.max_length - len(tokenized_code))
+        tokens = tokenized_code + padding
+        return tokens
